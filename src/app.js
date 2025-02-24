2ba9112700781bea96b944325508aebce2ba1d82
Shreyansh Pachouri <pachourishreyansh@gmail.com>
2025-02-24 15:56:56 +0530

    Made changes

--- src/app.js
+++ src/app.js
@@ -14,13 +14,7 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-app.use((req, res, next) => {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
-    next();
-});
-
-
-import userRouter from "./routes/user.routes.js";
+import userRouter from "./routes/user.routes.js"
 app.use("/api/v1/users", userRouter);
 
 export {app}
